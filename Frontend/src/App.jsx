801947af2d4092cc278c35e3204986bec3776857
Tanmay Maji <tmaji435@gmail.com>
2024-08-06 13:17:50 +0530

    frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,29 @@
-import PageRoute from "./routes/route"
-
-function App() {
-  return (
-    <>
-    <PageRoute/>
-    </>
-  )
+import Home from "./pages/home";
+import Quote from "./pages/quote";
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import NewQuote from "./pages/newQuote";
+import Display from "./components/displayquote";
+export default function App() {
+    return(
+        <>
+    <BrowserRouter>
+     <Routes>
+      <Route exact
+      path="/" element={<Home/>}/>
+      <Route
+      path="/Quote" element={<Quote/>}/>
+     <Route
+      path="/SignIn" element={<SignIn/>}/>
+      <Route
+      path="/SignUp" element={<SignUp/>}/>
+      <Route
+      path="/Newquote" element={<NewQuote/>}/>
+      <Route
+      path="/displayquote" element={<Display/>}/>
+    </Routes>
+    </BrowserRouter>
+        </>
+    )
 }
-
-export default App
