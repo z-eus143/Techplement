ee37d1fa252e131a000530002c0140af7967a58e
Tanmay Maji <tmaji435@gmail.com>
2024-08-08 10:34:35 +0530

    a:

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import Home from "./pages/home";
 import Quote from "./pages/quote";
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
-import { Analytics } from "@vercel/analytics/react"
 import SignIn from "./pages/signin";
 import SignUp from "./pages/signup";
 import NewQuote from "./pages/newQuote";
@@ -9,7 +8,7 @@ import Display from "./components/displayquote";
 import Search from "./pages/search";
 export default function App() {
     return(
-    <Analytics>
+        <>
     <BrowserRouter>
      <Routes>
       <Route exact
@@ -28,6 +27,6 @@ export default function App() {
       path="/search" element={<Search/>}/>
     </Routes>
     </BrowserRouter>
-    </Analytics>
+        </>
     )
 }
